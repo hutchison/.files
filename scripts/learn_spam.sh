cf650d8dd7afdd22e57ef06d682998aeb78c46f1
Martin Darmüntzel <martin@trivialanalog.de>
2018-09-06 12:29:44 +0200

    Verbessert learn_spam.sh.

--- scripts/learn_spam.sh
+++ scripts/learn_spam.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for f in ~/users/martin/.Spam/cur/*; do
-	spamc -L spam -U ~/tmp/spamd.sock < "$f"
-	grep -h ^Subject: "$f"
+for dir in ~/users/*; do
+	[ -e "$dir" ] || continue
+	for f in $dir/.Spam/cur/*; do
+		[ -e "$f" ] || continue
+		spamc -L spam -U ~/tmp/spamd.sock < "$f"
+		grep -h ^Subject: "$f"
+	done
 done
