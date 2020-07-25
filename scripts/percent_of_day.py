bb1898f98d764f4bf720dc93b5d34fe792378072
Martin Darmüntzel <martin@trivialanalog.de>
2020-07-25 16:16:30 +0200

    Verbessert percent_of_day.py.

--- scripts/percent_of_day.py
+++ scripts/percent_of_day.py
@@ -6,12 +6,8 @@ from datetime import datetime
 
 def percent_of_day(dt):
     elapsed_seconds = dt.hour*60*60 + dt.minute*60 + dt.second
-    return round(100*elapsed_seconds / (24*60*60), 2)
-
-
-def main():
-    print(percent_of_day(datetime.now()))
+    return 100*elapsed_seconds / (24*60*60)
 
 
 if __name__ == '__main__':
-    main()
+    print('{:.2f}'.format(percent_of_day(datetime.now())))
