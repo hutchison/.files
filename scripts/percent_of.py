c2079b961b3458531853f894cf32cd68a69dce0a
Martin Darmüntzel <martin@trivialanalog.de>
2021-12-23 10:03:47 +0100

    Erweitert percent_of.py um die Blockansicht.
    Und fügt Standardwerte für Argumente ein.

--- scripts/percent_of.py
+++ scripts/percent_of.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from datetime import datetime, timedelta
+from math import floor
 import argparse
 import calendar
-from datetime import datetime, timedelta
 import sys
 
 
@@ -15,12 +16,12 @@ def percent_remaining(start, dt, end):
     return 100 * (1 - diff_seconds(dt, end)/diff_seconds(start, end))
 
 
-def percent_of_day(dt):
+def percent_of_day(dt=datetime.now()):
     elapsed_seconds = dt.hour*60*60 + dt.minute*60 + dt.second
     return 100 * (elapsed_seconds / (24*60*60))
 
 
-def percent_of_week(dt):
+def percent_of_week(dt=datetime.now()):
     _date = dt.date()
     diff_to_most_recent_monday = timedelta(days=_date.weekday())
     d_monday = _date - diff_to_most_recent_monday
@@ -31,40 +32,61 @@ def percent_of_week(dt):
     return percent_remaining(start, dt, end)
 
 
-def percent_of_month(dt):
+def percent_of_month(dt=datetime.now()):
     start = datetime(dt.year, dt.month, 1, 0, 0, 0)
     _, last_day_of_month = calendar.monthrange(dt.year, dt.month)
     end = datetime(dt.year, dt.month, last_day_of_month, 23, 59, 59)
     return percent_remaining(start, dt, end)
 
 
-def percent_of_year(dt):
+def percent_of_year(dt=datetime.now()):
     start = datetime(dt.year, 1, 1, 0, 0, 0)
     end = datetime(dt.year, 12, 31, 23, 59, 59)
     return percent_remaining(start, dt, end)
+
+
+BLOCKS = [chr(9608+i) for i in range(8)]
+
+def blocky(p):
+    r = ''
+    if 0 <= p <= 100:
+        t, o = divmod(p, 10)
+        b_full = BLOCKS[0]
+        r = floor(t)*b_full
+        for i in range(8, 0, -1):
+            if o <= 10*i//8:
+                r += BLOCKS[-i]
+                break
+
+    r += (10-len(r)) * ' '
+    return r
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('unit', choices=['day', 'week', 'month', 'year'])
     parser.add_argument('--precision', type=int, default=0, required=False)
+    parser.add_argument('-b', '--block', action='store_true')
 
     args = parser.parse_args()
 
-    _now = datetime.now()
+    as_block = args.block
 
     if args.unit == 'day':
-        percentage = percent_of_day(_now)
+        percentage = percent_of_day()
     elif args.unit == 'week':
-        percentage = percent_of_week(_now)
+        percentage = percent_of_week()
     elif args.unit == 'month':
-        percentage = percent_of_month(_now)
+        percentage = percent_of_month()
     elif args.unit == 'year':
-        percentage = percent_of_year(_now)
+        percentage = percent_of_year()
 
     prefix = args.unit[0].upper()
 
     width = 2+args.precision
     fmt_str = prefix + ' {:' + str(width) + '.' + str(args.precision) + 'f}'
 
-    print(fmt_str.format(percentage))
+    if as_block:
+        print(blocky(percentage))
+    else:
+        print(fmt_str.format(percentage))
