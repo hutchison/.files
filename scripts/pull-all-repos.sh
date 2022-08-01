15c291f1c2f4a66bc64eeb4f90c49de548af0426
Martin Darmüntzel <martin@trivialanalog.de>
2022-08-01 13:16:24 +0200

    Fügt pull-all-repos.sh hinzu.

---
+++ scripts/pull-all-repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for dir in */; do
+	if [[ -d "$dir/.git" ]]; then
+		nr_remotes=$(git -C "$dir" remote | wc -l | xargs)
+		if [[ $nr_remotes > 0 ]]; then
+			echo "$dir"
+			git -C "$dir" pull
+		fi
+	fi
+done
