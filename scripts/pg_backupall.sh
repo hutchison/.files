0059709235a6869f0f4880f99f7e2530ecaad5ba
Martin Darmüntzel <martin@trivialanalog.de>
2023-01-23 16:59:22 +0100

    Fügt pg_backupall.sh hinzu.

---
+++ scripts/pg_backupall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+CURRENT_DATE=$(date +"%FT%T")
+OUTPUT_FILENAME="backup_all_${CURRENT_DATE}.sql"
+
+sudo -u postgres pg_dumpall > "$OUTPUT_FILENAME"
+xz "$OUTPUT_FILENAME"
+rm "$OUTPUT_FILENAME"
