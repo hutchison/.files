c9ea4d270aef7b4146426cf768a6a2179c3559b1
Martin Darmüntzel <martin@trivialanalog.de>
2023-05-31 16:13:15 +0200

    Ersetzt pg_backupall.sh durch das bessere backup_postgres_db.sh.

---
+++ scripts/backup_postgres_db.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# Export die Datenbank $DBNAME ($1) als SQL-Datei
+# in das Verzeichnis $DESTDIR ($2)
+# mit dem Präfix $PREFIX ($3)
+
+# Mittels "psql $DBNAME < $DESTFILENAME" kann diese importiert werden.
+# Vorher muss die alte Datenbank gelöscht werden:
+# $ drop_db $DBNAME
+# $ sudo -u postgres createdb -O $USER bpcupid
+
+usage() {
+	echo "$0 dbname destdir prefix"
+	exit 1
+}
+
+main() {
+	if [[ "$#" -ne 3 ]]; then
+		usage
+	else
+		DBNAME="$1"
+		DESTDIR="$2"
+		PREFIX="$3"
+
+		CURRENT_DATETIME=$(date +"%Y%m%d_%H%M%S")
+		DESTFILENAME="${PREFIX}_${DBNAME}_${CURRENT_DATETIME}.sql"
+
+		pg_dump "$DBNAME" > "${DESTDIR}/${DESTFILENAME}"
+		xz "${DESTDIR}/${DESTFILENAME}"
+	fi
+}
+
+main
