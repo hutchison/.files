2ce55c65a7ca355916aed62bd4b95b7a97e64fe8
Martin Darmüntzel <martin@trivialanalog.de>
2023-06-01 09:46:04 +0200

    Verbessert die Dokumentation von backup_postgres_db.sh.

--- scripts/backup_postgres_db.sh
+++ scripts/backup_postgres_db.sh
@@ -4,10 +4,18 @@
 # in das Verzeichnis $DESTDIR ($2)
 # mit dem Präfix $PREFIX ($3)
 
+# Damit die Datenbank exportiert werden kann, braucht der ausführende Nutzer
+# $username Zugriff:
+# sudo -u postgres psql $DBNAME
+# $DBNAME=# GRANT CONNECT ON DATABASE database_name TO username;
+# $DBNAME=# GRANT USAGE ON SCHEMA schema_name TO username;
+# GRANT SELECT ON ALL TABLES IN SCHEMA schema_name TO username;
+# GRANT SELECT ON ALL SEQUENCES IN SCHEMA schema_name TO username;
+
 # Mittels "psql $DBNAME < $DESTFILENAME" kann diese importiert werden.
 # Vorher muss die alte Datenbank gelöscht werden:
 # $ drop_db $DBNAME
-# $ sudo -u postgres createdb -O $USER bpcupid
+# $ sudo -u postgres createdb -O $USER $DBNAME
 
 usage() {
 	echo "$0 dbname destdir prefix"
